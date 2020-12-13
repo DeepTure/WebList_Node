c20b0115174161d9c68a176c283f924f075989d1
CuellarBetancourtDaniel <61211097+CuellarBetancourtDaniel@users.noreply.github.com>
2020-12-13 08:06:50 -0600

    CRUD alumno

--- routes/crud.alumno.routes.js
+++ routes/crud.alumno.routes.js
@@ -74,3 +74,106 @@ router.get("/datos", (req, res) => {
         usa el segundo metodo
         */
 });
+
+
+const router = require("express").Router();
+const db = require("../database/connection");
+
+//url de los alumnos
+router.get("[aqui va la direccion]", (req, res) => {
+    //Se realiza la consulta
+    db.query("select * from alumnos", (err, resul) => {
+        //en caso de que ocurra error
+        if (err) {
+            console.log(err);
+        } else {
+            //Pase de parametros al url y mostrarlos en la página
+            return res.render("[aqui va la dirección]", {
+                profesores: resul,
+            });
+        }
+    });
+});
+//Funcion de añadir alumno con el formulario
+router.post("[nombre del formulario]", (req, res) => {
+    //guardamos los datos que recibimos de la vista
+    var { boleta, nombre, app, apm, correo, contraseña } = req.body;
+    //Sentencia sql
+    db.query(
+        "insert into Profesores set ?",
+        {
+            boleta,
+            nombre,
+            app,
+            apm,
+            correo,
+            contraseña,
+        },
+        (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                //direcciónar a la pagina si el proceso fue correcto
+                return res.redirect("[aqui va la direccion]");
+            }
+        }
+    );
+});
+
+//Funcion de modificar todos los datos de un alumno mediante la boleta
+router.post("[nombre del formulario]", (req, res) => {
+    //datos que se reciben  de la vista
+    var { boleta, nombre, app, apm, correo, contraseña } = req.body;
+    var modificacion = { nombre, app, apm, contraseña, correo };
+    //Sentencia sql
+    db.query(
+        "update alumnos set? where boleta=?",
+        [modificacion, boleta],
+        (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                //direcciónar a la pagina si el proceso fue correcto
+                return res.redirect("[aqui va la direccion]");
+            }
+        }
+    );
+});
+
+//Eliminar a un alumno 
+router.get("/eliminar/:boleta", (req, res) => {
+    var { boleta } = req.params;
+    conexion.query(
+        "delete from alumnos where boleta= ?",
+        [boleta],
+        (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                //direcciónar a la pagina si el proceso fue correcto
+                return res.redirect("[aqui va la direccion]");
+            }
+        }
+    );
+});
+
+//Eliminar alumno mediante formulario
+router.post("[nombre del formulario]", (req, res) => {
+    //guardamos los datos que recibimos de la vista
+    var { boleta } = req.body;
+    //Sentencia sql
+    db.query(
+        "delete from Profesores where boleta= ?",
+        [boleta],
+        (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                //direcciónar a la pagina si el proceso fue correcto
+                res.redirect("[aqui va la direccion]");
+            }
+        }
+    );
+});
+
+module.exports = router;
