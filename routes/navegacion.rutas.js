7731704ad18ed3cbaea100f5f2b66fdb9b5d0b7f
LeoYeudiel <lyeuprsw@gmail.com>
2020-12-28 19:47:21 -0600

    sesiones sin base de datos

--- routes/navegacion.rutas.js
+++ routes/navegacion.rutas.js
@@ -3,9 +3,54 @@
 *al menos de manera provicional
 */
 const router = require("express").Router();
+const db = require("../database/connection");
+const passport = require('passport');
+const cookieParser=require('cookie-parser');
+const session= require('express-session');
+const Passportlocal = require('passport-local').Strategy;
 
-router.get('/home',(req,res)=>{
+
+router.use(cookieParser('msj'));
+router.use(session({
+    secret: 'msj',
+    resave: true,
+    saveUninitialized: true
+}));
+
+router.use(passport.initialize());
+router.use(passport.session());
+
+
+passport.use(new Passportlocal(function(username,password,done){
+    if(username === "Leo" && password === "root"){
+        return done (null, {id: 1, name: "Leonidas"}); 
+    }else{ 
+    done(null,false);
+    }
+}));
+
+passport.serializeUser(function(user,done){
+    done(null,user.id);
+});
+
+passport.deserializeUser(function(id,done){
+    done(null, {id:1, name: "Leonidas"});
+});
+
+router.post("/InicioSesionController", passport.authenticate('local',{
+    successRedirect:"/home",
+    failureRedirect: "/"
+}));
+
+router.get("/home", (req,res,next)=>{
+    if(req.isAuthenticated()) return next();
+
+
+    res.redirect("/");
+},(req,res)=>{
+
     return res.render('Home',{});
+    
 });
 
 module.exports = router;
