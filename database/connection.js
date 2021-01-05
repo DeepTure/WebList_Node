d102245e5818362d1d2238d3a4acfad06b5f9bb1
cdelaof26 <50687577+cdelaof26@users.noreply.github.com>
2021-01-05 15:48:16 -0600

    Update connection.js

--- database/connection.js
+++ database/connection.js
@@ -4,7 +4,7 @@ const mysql = require("mysql");
 const mysqlConnection = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "alpine",
+    password: "n0mel0",
     database: "weblist",
     multipleStatements: true,
 });
