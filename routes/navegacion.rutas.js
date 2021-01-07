01a6b5c92a3c430e050220db5218b03132607b07
jafet5757 <moranorozcokevinjafet@gmail.com>
2021-01-07 14:42:32 -0600

    Correo de recuperacion terminado

--- routes/navegacion.rutas.js
+++ routes/navegacion.rutas.js
@@ -12,21 +12,15 @@ router.get('/error',(req,res)=>{
 //para enviar correos
 const nodemailer = require('nodemailer');
 
-router.get('/send-email',(req,res)=>{
-    let transporter = nodemailer.createTransport({
-        service:'gmail',
-        auth: {
-            user:'jafetkevin575@gmail.com',
-            pass:'6!T3UzpT'
-        }
-    });
-
-    let mailOptions = {
-        from:'Remitente',
-        to:'moran.orozco.kevin@gmail.com',
-        subject:'Enviado desde nodemailer',
-        text:'Hola mundo!'
+const transporter = nodemailer.createTransport({
+    service:'gmail',
+    auth: {
+        user:'jafetkevin575@gmail.com',
+        pass:'6!T3UzpT'
     }
+});
+
+router.get('/send-email',(req,res)=>{
 
     transporter.sendMail(mailOptions,(error,info)=>{
         if(error){
@@ -37,5 +31,59 @@ router.get('/send-email',(req,res)=>{
         }
     });
 });
+
+router.get('/recover',(req,res)=>{
+    res.render('Recover',{correo:'null'});
+});
+
+router.post('/comprobateEmail',(req,res)=>{
+    const data = req.body;
+
+        db.query('SELECT contraseña FROM profesor WHERE correo=?',[data.correo],(err,numEmpleado)=>{
+            if(err)res.json(err)
+            if(Array.isArray(numEmpleado) && !(numEmpleado.length === 0)){
+                enviarCorreo(data.correo, numEmpleado[0].contraseña);
+                res.render('Recover',{correo:'Se le ha enviado un correo con su contraseña',icon:'success'});
+            }else{
+                db.query('SELECT contraseña FROM alumno WHERE correo=?',[data.correo],(err,boleta)=>{
+                    if(err)res.json(err)
+                    if(Array.isArray(boleta) && !(boleta.length === 0)){
+                        enviarCorreo(data.correo, boleta[0].contraseña);
+                        res.render('Recover',{correo:'Se le ha enviado un correo con su contraseña',icon:'success'});
+                    }else{
+                        db.query('SELECT contraseña FROM administrador WHERE correo=?',[data.correo],(err,id)=>{
+                            if(err)res.json(err)
+                            if(Array.isArray(id) && !(id.length === 0)){
+                                enviarCorreo(data.correo, id[0].contraseña);
+                                res.render('Recover',{correo:'Se le ha enviado un correo con su contraseña',icon:'success'});
+                            }else{
+                                res.render('Recover',{correo:'No hemos encontrado su correo',icon:'error'});
+                            }
+                        });
+                    }
+                });
+            }
+        })
+
+});
+
+function enviarCorreo(correo, contrasena){
+    const mailOptions = {
+        from: 'System',
+        to: correo,
+        subject: 'Recuperacion de contraseña',
+        text: 'Su contraseña es: '+contrasena
+      };
+    
+      transporter.sendMail(mailOptions,(error,info)=>{
+        if(error){
+            res.status(500).send(error.message);
+
+        }else{
+            console.log('Email enviado');
+            res.send('Correo enviado')
+        }
+    });
+}
 
 module.exports = router;
